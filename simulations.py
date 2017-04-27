bc18d17588690ea91950f3fe49d9f4f3227c71f2
Jason <jbertram@email.arizona.edu>
2017-04-26 18:45:33 -0700

    latest

--- simulations.py
+++ simulations.py
@@ -164,7 +164,7 @@ def d(t):
 c=np.array([1.,1.])
 T=100000
 g=40.     #iterations per seasonal cycle
-bparam=np.array([[0.0,0.0],[.5,.2]])
+bparam=np.array([[0.0,0.0],[.5,.217]])
 dparam=np.array([[0.0,0.0],[0.2,0.1]])
 totaltime=100
 
@@ -175,16 +175,6 @@ for t in range(totaltime):
     n = n + deltnplus((b(t)*n*U/T).astype(np.int),c,U.astype(np.int))-d(t)*n
     nhist.append(list(n))
     
-#n=np.array([60000.,25000.])
-#nhistclassic=[n];
-#for t in range(totaltime):
-#    U=T-sum(n)
-#    n = n + deltnplusclassic((b(t)*n*U/T).astype(np.int),c,U.astype(np.int))-d(t)*n
-#    nhistclassic.append(list(n))
-#
-#plt.stackplot(range(totaltime+1),np.array(nhistclassic)[:,1],np.array(nhistclassic)[:,0],colors=['b','k'])
-#plt.plot(np.array(nhistclassic)[:,0]/np.sum(nhistclassic,1),'k',lw=2)
-    
     
 #n=np.array([6000.,2500.])
 #nsimhist=[n]
@@ -193,7 +183,59 @@ for t in range(totaltime):
 #    n = n + sum(deltnplussim((b(t)*n*U/T).astype(np.int),c,U.astype(np.int)))-d(t)*n
 #    nsimhist.append(list(n))
 
-fig=plt.subplot(311)
+fig=plt.subplot(321)
+for t in range(0,100,40):
+    fig.plot(range(t,t+20),[dparam[1][0] for _ in range(t,t+20)],'k.',lw=2)
+    fig.plot(range(t,t+20),[dparam[1][1] for _ in range(t,t+20)],'b.',lw=2)
+    
+fig.fill([20,40,40,20],[0,0,1,1],'g',alpha=0.3,edgecolor='none')
+fig.fill([60,40+40,40+40,60],[0,0,1,1],'g',alpha=0.3,edgecolor='none')
+fig.plot(range(20,40),[bparam[1][0] for _ in range(t,t+20)],'k.',lw=2)
+fig.plot(range(20,40),[bparam[1][1] for _ in range(t,t+20)],'b.',lw=2)
+fig.plot(range(60,80),[bparam[1][0] for _ in range(t,t+20)],'k.',lw=2,label=r"$b$ spec.")
+fig.plot(range(60,80),[bparam[1][1] for _ in range(t,t+20)],'b.',lw=2,label=r"$d$ spec.")
+
+fig.set_xticklabels([])
+fig.set_xlim([0,100])
+fig.set_ylim([0,0.7])
+plt.ylabel(r"$b(t), d(t)$",fontsize=14)
+fig.annotate(r'$(a)$',xy=(0.01,0.9),xycoords='axes fraction',fontsize=12)
+fig.annotate(r'$d$',xy=(0.09,0.4),xycoords='axes fraction',fontsize=16)
+fig.annotate(r'$b$',xy=(0.29,0.4),xycoords='axes fraction',fontsize=16)
+fig.annotate(r'$d$',xy=(0.49,0.4),xycoords='axes fraction',fontsize=16)
+fig.annotate(r'$b$',xy=(0.69,0.4),xycoords='axes fraction',fontsize=16)
+fig.annotate(r'$d$',xy=(0.89,0.4),xycoords='axes fraction',fontsize=16)
+
+fig=plt.subplot(323)
+plt.stackplot(range(totaltime+1),np.array(nhist)[:,1],np.array(nhist)[:,0],colors=['b','k'])
+fig.set_xticklabels([])
+fig.set_yticklabels(['0.0','0.2','0.4','0.6','0.8','1.0'])
+plt.ylabel(r"Abundance$/T$",fontsize=14)
+fig.annotate(r'$(b)$',xy=(0.01,0.9),xycoords='axes fraction',fontsize=12)
+
+fig=plt.subplot(325)
+plt.stackplot(range(totaltime+1),np.array(nhist)[:,1]/np.sum(nhist,1),np.array(nhist)[:,0]/np.sum(nhist,1),colors=['b','k'])
+plt.ylim([0,1])
+plt.xlabel(r"$t$",fontsize=14)
+plt.ylabel("Frequency",fontsize=14)
+fig.annotate(r'$(c)$',xy=(0.01,0.9),xycoords='axes fraction',color='white',fontsize=12)
+
+#Classic lottery
+c=np.array([1.,1.])
+T=100000
+g=40.     #iterations per seasonal cycle
+bparam=np.array([[0.0,0.0],[.5,.0421]])
+dparam=np.array([[0.0,0.0],[0.2,0.1]])
+totaltime=100
+
+n=np.array([60000.,25000.])
+nhistclassic=[n];
+for t in range(totaltime):
+    U=T-sum(n)
+    n = n + deltnplusclassic((b(t)*n*U/T).astype(np.int),c,U.astype(np.int))-d(t)*n
+    nhistclassic.append(list(n))
+    
+fig=plt.subplot(322)
 for t in range(0,100,40):
     fig.plot(range(t,t+20),[dparam[1][0] for _ in range(t,t+20)],'k.',lw=2)
     fig.plot(range(t,t+20),[dparam[1][1] for _ in range(t,t+20)],'b.',lw=2)
@@ -206,33 +248,31 @@ fig.plot(range(60,80),[bparam[1][0] for _ in range(t,t+20)],'k.',lw=2,label=r"$b
 fig.plot(range(60,80),[bparam[1][1] for _ in range(t,t+20)],'b.',lw=2,label=r"$d$ specialist")
 
 fig.set_xticklabels([])
+fig.set_yticklabels([])
 fig.set_xlim([0,100])
 fig.set_ylim([0,0.7])
-plt.ylabel(r"$b(t), d(t)$",fontsize=14)
-fig.annotate(r'$(a)$',xy=(0.01,0.9),xycoords='axes fraction',fontsize=12)
+fig.annotate(r'$(d)$',xy=(0.01,0.9),xycoords='axes fraction',fontsize=12)
 fig.annotate(r'$d$',xy=(0.09,0.4),xycoords='axes fraction',fontsize=16)
 fig.annotate(r'$b$',xy=(0.29,0.4),xycoords='axes fraction',fontsize=16)
 fig.annotate(r'$d$',xy=(0.49,0.4),xycoords='axes fraction',fontsize=16)
 fig.annotate(r'$b$',xy=(0.69,0.4),xycoords='axes fraction',fontsize=16)
 fig.annotate(r'$d$',xy=(0.89,0.4),xycoords='axes fraction',fontsize=16)
-plt.legend(loc='upper center',prop={'size':10})
+plt.legend(loc='upper right',prop={'size':10})
 
-fig=plt.subplot(312)
-plt.stackplot(range(totaltime+1),np.array(nhist)[:,1],np.array(nhist)[:,0],colors=['b','k'])
+fig=plt.subplot(324)
+plt.stackplot(range(totaltime+1),np.array(nhistclassic)[:,1],np.array(nhistclassic)[:,0],colors=['b','k'])
 fig.set_xticklabels([])
-fig.set_yticklabels(['0.0','0.2','0.4','0.6','0.8','1.0'])
-plt.ylabel(r"Abundance$/T$",fontsize=14)
-fig.annotate(r'$(b)$',xy=(0.01,0.9),xycoords='axes fraction',fontsize=12)
+fig.set_yticklabels([])
+fig.annotate(r'$(e)$',xy=(0.01,0.9),xycoords='axes fraction',fontsize=12)
 
-fig=plt.subplot(313)
-plt.stackplot(range(totaltime+1),np.array(nhist)[:,1]/np.sum(nhist,1),np.array(nhist)[:,0]/np.sum(nhist,1),colors=['b','k'])
+fig=plt.subplot(326)
+plt.stackplot(range(totaltime+1),np.array(nhistclassic)[:,1]/np.sum(nhistclassic,1),np.array(nhistclassic)[:,0]/np.sum(nhistclassic,1),colors=['b','k'])
 plt.ylim([0,1])
 plt.xlabel(r"$t$",fontsize=14)
-plt.ylabel("Frequency",fontsize=14)
-fig.annotate(r'$(c)$',xy=(0.01,0.9),xycoords='axes fraction',fontsize=12)
+fig.set_yticklabels([])
+fig.annotate(r'$(f)$',xy=(0.01,0.9),xycoords='axes fraction',color='white',fontsize=12)
 
 plt.savefig('/home/jbertram/repos/densitydependentlottery/fluctuatingselection.pdf')
-
 
 #coexistence 
 #=================================================================
