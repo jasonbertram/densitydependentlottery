00384369e2a504b6f7d9ec049327ed7dabef45fa
Jason <jbertram@email.arizona.edu>
2018-04-04 12:06:00 -0700

    l

--- simulation_comparison.py
+++ simulation_comparison.py
@@ -10,7 +10,7 @@ Created on Wed Nov 29 12:26:08 2017
 c=np.array([1.5,1.])
 cbar=sum(np.array([0.1,0.9])*c)
 U=100000
-ls=np.array([[x*0.1,x] for x in np.linspace(0.01,20,1000)])
+ls=np.array([[x*0.1,x] for x in np.linspace(0.01,20,50)])
 
 exact=np.array([deltnplussim(m,c,U) for m in U*ls]);
 totalclassic=np.array([deltnplusclassic(m,c,U) for m in U*ls]);
@@ -28,10 +28,10 @@ ax1.plot(ls[:,0],comp1,'r',label=r"$R_i \frac{c_i}{\overline{c}}$")
 ax1.plot(ls[:,0],comp2,'b',label=r"$A_i \frac{c_i}{\overline{c}}$")
 ax1.plot(ls[:,0],(nocomp+comp1+comp2),'k',label=r"$e^{-L}+R_i \frac{c_i}{\overline{c}}+A_i \frac{c_i}{\overline{c}}$",linewidth=2)
 
-ax1.plot(ls[:,0],(exact[:,0,0]+exact[:,1,0]+exact[:,2,0])/(ls[:,0]*U),'k.',markersize=1.,label="Simulation")
-ax1.plot(ls[:,0],exact[:,0,0]/(ls[:,0]*U),'k.',markersize=1)
-ax1.plot(ls[:,0],exact[:,1,0]/(ls[:,0]*U),'k.',markersize=1)
-ax1.plot(ls[:,0],exact[:,2,0]/(ls[:,0]*U),'k.',markersize=1)
+ax1.plot(ls[:,0],(exact[:,0,0]+exact[:,1,0]+exact[:,2,0])/(ls[:,0]*U),'ko',markersize=6,markerfacecolor='None',label="Simulation")
+ax1.plot(ls[:,0],exact[:,0,0]/(ls[:,0]*U),'ko',markersize=6,markerfacecolor='None')
+ax1.plot(ls[:,0],exact[:,1,0]/(ls[:,0]*U),'ko',markersize=6,markerfacecolor='None')
+ax1.plot(ls[:,0],exact[:,2,0]/(ls[:,0]*U),'ko',markersize=6,markerfacecolor='None')
 
 ax1.plot(ls[:,0],totalclassic[:,0]/(ls[:,0]*U),'k--',label=r"Classic lottery",linewidth=2)
 ax1.set_xlim([0,2])
@@ -54,7 +54,7 @@ ax2.plot(ls[:,1],nocomp,'k',label=r"$e^{-L}$")
 ax2.plot(ls[:,1],comp1,'r',label=r"$R_2 c_2/\overline{c}$")
 ax2.plot(ls[:,1],comp2,'b',label=r"$A_2 c_2/\overline{c}$")
 
-ax2.plot(ls[:,1],(exact[:,0,1]+exact[:,1,1]+exact[:,2,1])/(ls[:,1]*U),'k.',markersize=1,label="Simulation")
+ax2.plot(ls[:,1],(exact[:,0,1]+exact[:,1,1]+exact[:,2,1])/(ls[:,1]*U),'ko',markersize=6,markerfacecolor='None',label="Simulation")
 ax2.plot(ls[:,1],(nocomp+comp1+comp2),'k',label="Total",linewidth=2)
 ax2.plot(ls[:,1],totalclassic[:,1]/(ls[:,1]*U),'k--',label=r"Classic lottery",linewidth=2)
 ax2.set_xlim([0,20])
@@ -65,12 +65,12 @@ ax2.set_xlabel(r"$L$",fontsize=14)
 ax2.set_xticklabels([0,'',10,'',20])
 
 
-ax2.plot(ls[:,1],exact[:,0,1]/(ls[:,1]*U),'k.',markersize=1,label="Simulation")
-ax2.plot(ls[:,1],exact[:,1,1]/(ls[:,1]*U),'k.',markersize=1)
-ax2.plot(ls[:,1],exact[:,2,1]/(ls[:,1]*U),'k.',markersize=1)
+ax2.plot(ls[:,1],exact[:,0,1]/(ls[:,1]*U),'ko',markersize=6,markerfacecolor='None',label="Simulation")
+ax2.plot(ls[:,1],exact[:,1,1]/(ls[:,1]*U),'ko',markersize=6,markerfacecolor='None')
+ax2.plot(ls[:,1],exact[:,2,1]/(ls[:,1]*U),'ko',markersize=6,markerfacecolor='None')
 
 ax2.annotate('Common type',xy=(0.6,0.32),xycoords='axes fraction',fontsize=12)
 
 plt.tight_layout()
 
-plt.savefig('/home/jason/repos/densitydependentlottery/simulationcomparison.pdf',bbox="tight")
+plt.savefig('/home/jbertram/repos/densitydependentlottery/simulationcomparison.pdf',bbox="tight")
